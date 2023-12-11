fd0515aa2a580daaaac91b9f903e93315e75f679
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-12-10 20:23:26 -0500

    Add csharp file

---
+++ a.cs
@@ -0,0 +1,23 @@
+using System; // this is for Console.WriteLine
+using System.Collections.Generic; // this is for Lists
+
+
+string[] groceryItems = {"bananas", "bread", "zucchini", "lemons"}
+
+
+string[] movies = new string[3];
+
+movies[0] = "Gladiator"
+movies[1] = "Donnie Darko"
+movies[2] = "Memento"
+
+
+
+
+List<string> shoppingList = new List<string>();
+shoppingList.Add("Dreams")
+shoppingList.Add("Rainbows")
+shoppingList.Add("Pony")
+
+shoppingList.Remove("Dreams"); // or shoppingList.RemoveAt(0);
+
